56029eb3fbddcdbd4e13bfff723f9da0d3d60cac
Guillermo Mazzola <gmazzo65@gmail.com>
2018-10-06 15:53:03 -0300

    Removed some code

--- app/src/main/java/org/test/kotlin/demo/repos/ReposViewHolder.java
+++ app/src/main/java/org/test/kotlin/demo/repos/ReposViewHolder.java
@@ -13,34 +13,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 class ReposViewHolder extends RecyclerView.ViewHolder {
     private final TextView name;
-    private final TextView nameLabel;
-    private final TextView url;
     private final TextView description;
-    private final TextView descriptionLabel;
 
-    public ReposViewHolder(@NonNull View view) {
+    ReposViewHolder(@NonNull View view) {
         super(view);
 
         this.name = itemView.findViewById(R.id.name);
-        this.nameLabel = itemView.findViewById(R.id.nameLabel);
-        this.url = itemView.findViewById(R.id.url);
         this.description = itemView.findViewById(R.id.description);
-        this.descriptionLabel = itemView.findViewById(R.id.descriptionLabel);
     }
 
     void bindItem(RepositoryDTO item) {
-        setText(name, nameLabel, item.getName());
-        setText(url, null, item.getRepoURL());
-        setText(description, descriptionLabel, item.getDescription());
+        setText(name, item.getName());
+        setText(description, item.getDescription());
     }
 
     // TODO duplicated with UsersViewHolder#setText
-    private void setText(@NonNull TextView value, @Nullable TextView label, @Nullable CharSequence text) {
+    private void setText(@NonNull TextView value, @Nullable CharSequence text) {
         value.setText(text);
         value.setVisibility(TextUtils.isEmpty(text) ? View.GONE : View.VISIBLE); // TODO this could be generic?
-        if (label != null) {
-            label.setVisibility(value.getVisibility());
-        }
     }
 
 }
--- app/src/main/java/org/test/kotlin/demo/users/UsersViewHolder.java
+++ app/src/main/java/org/test/kotlin/demo/users/UsersViewHolder.java
@@ -13,23 +13,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 class UsersViewHolder extends RecyclerView.ViewHolder {
     private final TextView name;
-    private final TextView nameLabel;
     private final TextView url;
     private final TextView user;
-    private final TextView userLabel;
     private final TextView info;
-    private final TextView infoLabel;
 
     public UsersViewHolder(@NonNull View view) {
         super(view);
 
         this.name = itemView.findViewById(R.id.name);
-        this.nameLabel = itemView.findViewById(R.id.nameLabel);
         this.url = itemView.findViewById(R.id.url);
         this.user = itemView.findViewById(R.id.user);
-        this.userLabel = itemView.findViewById(R.id.userLabel);
         this.info = itemView.findViewById(R.id.info);
-        this.infoLabel = itemView.findViewById(R.id.infoLabel);
     }
 
     void bindItem(UserDTO item) {
@@ -38,18 +32,15 @@ class UsersViewHolder extends RecyclerView.ViewHolder {
                 item.getRepositories(),
                 item.getRepositories());
 
-        setText(user, userLabel, item.getUser());
-        setText(url, null, item.getProfileURL());
-        setText(name, nameLabel, item.getName());
-        setText(info, infoLabel, infoValue);
+        setText(user, item.getUser());
+        setText(url, item.getProfileURL());
+        setText(name, item.getName());
+        setText(info, infoValue);
     }
 
-    private void setText(@NonNull TextView value, @Nullable TextView label, @Nullable CharSequence text) {
+    private void setText(@NonNull TextView value, @Nullable CharSequence text) {
         value.setText(text);
         value.setVisibility(TextUtils.isEmpty(text) ? View.GONE : View.VISIBLE); // TODO this could be generic?
-        if (label != null) {
-            label.setVisibility(value.getVisibility());
-        }
     }
 
 }
